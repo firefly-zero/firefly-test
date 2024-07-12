7d84294313e0a3d0c0c5b87e95ed3e17a0bb0d7e
gram <git@orsinium.dev>
2024-07-12 18:50:23 +0200

    add Runner class

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct Runner {
+    num: i32,
 }
 
-/// A Python module implemented in Rust.
+#[pymethods]
+impl Runner {
+    #[new]
+    fn new(num: i32) -> Self {
+        Runner { num }
+    }
+}
+
+/// Framework for testing Firefly Zero games.
 #[pymodule]
 fn firefly_test(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Runner>()?;
     Ok(())
 }
